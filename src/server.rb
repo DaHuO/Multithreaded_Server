6141e5bce1aa6c264f55172f5a0f3bb7712985d8
[DaHuO] <[dhuo@tcd.ie]>
2015-10-29 22:28:01 +0000

    bugs

--- src/server.rb
+++ src/server.rb
@@ -1,40 +1,44 @@
 load "ThreadPool.rb"
 require 'socket'
+require 'net/http'
 
-def handle_client(c, count)
+def handle_client(c, count, server)
 	student_id = 'oldk'
 	while input = c.gets
 		puts 'got one'
 		p c.remote_address.ip_address
-		arg = "It is executed by thread #{Thread.current[:id]}\n" +
-			"The origin message is #{input}"
-		# if input == "HELO text\n"
-		# 	arg = "HELO text\nIP:[#{c.remote_address.ip_address}]\n" +
-		# 	"Port:[#{c.remote_address.ip_port}]" +
-		# 	"\nStudentID:[#{student_id}]"
-		# end
+		arg = input
+
 		puts "one to four #{input[0,4]}"
 		if input[0,4] == "HELO"
-			arg = "#{input}"+"IP:#{c.remote_address.ip_address}\n" +
-			"Port:#{c.remote_address.ip_port}" +
+			arg = "#{input}"+"IP:#{$ip}\n" +
+			"Port:#{$port}" +
 			"\nStudentID:#{student_id}"
-			puts "got there"
 		end
 		puts "client_#{count} is coming with #{input}"
 		puts "the message sent back is \n<\n#{arg}\n>"
 		puts "It is being executed by thread #{Thread.current[:id]}"
 		c.puts (arg)
 		if input == "KILL_SERVICE\n"
+			c.close
+			server.close
 			raise SystemExit
+			
 		end
 	end
 end
-
-port = 3457 
-if ARGV.length != 0 
-	port = ARGV[0].to_i
+uri = URI('http://ipecho.net/plain')
+body = Net::HTTP.get(uri)
+if body.length!=0
+	$ip = body
+else
+	$ip = '127.0.0.1'
 end
-server = TCPServer.open(port)
+$port = '3457'
+if ARGV.length != 0 
+	$port = ARGV[0]
+end
+server = TCPServer.open($port)
 thread_pool = ThreadPool.new(5)	#to create a thread pool with 10 threads
 puts 'got the thread_pool'
 count = 0
@@ -42,6 +46,6 @@ while true
 	client = server.accept
 	thread_pool.schedule(client) do |c|
 		count += 1
-		handle_client(c, count)
+		handle_client(c, count, server)
 	end
 end
