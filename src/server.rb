7b24500fd386fed19eb236d0b4289085caa0a959
[DaHuO] <[dhuo@tcd.ie]>
2015-11-05 15:43:32 +0000

    try to fix the not able kill service problem

--- src/server.rb
+++ src/server.rb
@@ -12,13 +12,13 @@ def handle_client(c, count, server)
 			arg = "#{input}"+"IP:#{$ip}\n" +
 			"Port:#{$port}" +
 			"\nStudentID:#{student_id}"
+			c.puts (arg)
 		end
 		puts "client_#{count} is coming with #{input}"
 		puts "the message sent back is \n<\n#{arg}\n>"
 		puts "It is being executed by thread #{Thread.current[:id]}"
-		c.puts (arg)
 		if input == "KILL_SERVICE\n"
-			# c.close
+			c.close
 			server.close
 			raise SystemExit			
 		end
@@ -31,6 +31,7 @@ if body.length!=0
 else
 	$ip = '127.0.0.1'
 end
+$ip = '127.0.0.1'
 if ARGV.length != 0 
 	$port = ARGV[0]
 end
